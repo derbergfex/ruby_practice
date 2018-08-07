71d4c0dc0db4bf46f91f6c54c1bfadfdf72d3748
Yussef Soudan <yussefsoudan@gmail.com>
2018-08-07 22:13:24 +0100

    caesar final

--- building_blocks/caesar.rb
+++ building_blocks/caesar.rb
@@ -2,7 +2,6 @@ def caesar_cipher(str, shift)
     arr = str.each_byte.to_a
     str = ""
     arr.each do |num|
-        
         if num.between?(97, 122)
             num = (((num % 97) + shift) % 26) + 97
         elsif num.between?(65, 90)
